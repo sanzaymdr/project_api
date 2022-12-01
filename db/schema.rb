eb313b77b06af6108b3cecff15b99c4b86aaa2ed
sanzay_mdr <sanzaymanandhar99@gmail.com>
2022-12-01 20:13:46 +0545

    feat: update schema file for project table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_070407) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_142524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "type"
+    t.string "location"
+    t.string "thumbnail"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -24,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_070407) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "projects", "users"
 end
