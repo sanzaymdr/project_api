74678c8ebd0965c92f458b9a5e40bebee02ffb5b
sanzay_mdr <sanzaymanandhar99@gmail.com>
2022-12-01 20:32:48 +0545

    feat: add model level validation in Project, User model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# Project model to handle project
 class Project < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :title, :type, :location, :thumbnail
+  validates_uniqueness_of :title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# User model to handle users
 class User < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :first_name, :last_name, :email, :password
+  validates_uniqueness_of :email
+
+  has_many :projects
 end
