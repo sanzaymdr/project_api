57b527a784a17940a8ff5ae5a2a5da407a7feffc
sanzay_mdr <sanzaymanandhar99@gmail.com>
2022-12-03 02:23:22 +0545

    fix: handle destroy of associated contents of a project and freeze the enum values

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,12 @@
 # Project model to handle project
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :contents
+  has_many :contents, dependent: :destroy
 
   validates_presence_of :title, :project_type, :location, :thumbnail
   validates_uniqueness_of :title, scope: :user
 
   mount_uploader :thumbnail, ThumbnailUploader
 
-  enum project_type: { in_house: 0, external: 1, international: 2 }
+  enum project_type: { in_house: 0, external: 1, international: 2 }.freeze
 end
